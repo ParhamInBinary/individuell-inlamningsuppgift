36716ffc6f0830a1c8e5e433e2af30fef8e9fa0f
ParhamInBinart <parham.berenjian@gmail.com>
2023-11-24 21:42:28 +0100

    header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material';
+
+import { HeaderContainer } from './styles';
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <Typography variant="h1">Dictionary</Typography>
+    </HeaderContainer>
+  );
+};
---
+++ src/components/header/styles.ts
@@ -0,0 +1,9 @@
+import { Box, styled } from '@mui/material';
+
+export const HeaderContainer = styled(Box)(() => ({
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '20px',
+}));
